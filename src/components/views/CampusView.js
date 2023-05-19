9e60ef8d184ab8677d7bee398ffdf8c4ad260974
Hazel H Pham <51035837+hazelhpham@users.noreply.github.com>
2023-05-18 21:50:25 -0400

    single student view + single campus view in progress

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -5,27 +5,51 @@ The Views component is responsible for rendering web page with data provided by 
 It constructs a React component to display a single campus and its students (if any).
 ================================================== */
 import { Link } from "react-router-dom";
+import { deleteCampus } from "../../store/actions/actionCreators";
 
 // Take in props data to construct the component
 const CampusView = (props) => {
-  const {campus} = props;
-  
+  const { campus } = props;
+  const { deleteCampus } = props;
+
   // Render a single Campus view with list of its students
+  //   Single Campus View
+  // 3. can navigate to the Single Campus View, and
+  // ○ see details about a single campus, with all data fields, including enrolled students (if any)
+  // ○ see an informative message if no students are enrolled at that campus
+  // ○ navigate to the Single Student View and see any student's information
+  // ○ add new/existing students to the campus (e.g., via link/button)
+  // ○ delete students from the campus (e.g., via link/button)
+  // ○ delete the campus
   return (
     <div>
       <h1>{campus.name}</h1>
       <p>{campus.address}</p>
       <p>{campus.description}</p>
-      {campus.students.map( student => {
-        let name = student.firstname + " " + student.lastname;
-        return (
-          <div key={student.id}>
-            <Link to={`/student/${student.id}`}>
-              <h2>{name}</h2>
-            </Link>             
-          </div>
-        );
-      })}
+      <p>{campus.imageUrl}</p>
+
+      {campus.students.length > 0 ? (
+        campus.students.map((student) => {
+          let name = student.firstname + " " + student.lastname;
+          return (
+            <div key={student.id}>
+              <Link to={`/student/${student.id}`}>
+                <h2>{name}</h2>
+              </Link>
+            </div>
+          );
+        })
+      ) : (
+        <div>"No students enrolled in this school" </div>
+      )}
+      <Link to={`newstudent`}>
+        <button>Add New Student</button>
+      </Link>
+      {/* ○ navigate to the Edit Campus View and edit the campus information */}
+      <Link to={`editcampus`}>
+        <button>Edit campus</button>
+      </Link>
+      {/* <button onClick={() => deleteCampus(campus.id)}>Delete</button> */}
     </div>
   );
 };
--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,17 +4,30 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom";
+
 const StudentView = (props) => {
   const { student } = props;
 
-  // Render a single Student view 
+  // Render a single Student view
   return (
     <div>
-      <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      {student.campus.name !== null ? (
+        <>
+          <h1>{student.firstname + " " + student.lastname}</h1>
+          <h3>{student.campus.name}</h3>
+          <div key={student.campus.id}>
+            <Link to={`/campuses/${student.campus.id}`}>
+              <h2>{student.campus.name}</h2>
+            </Link>
+          </div>
+        </>
+      ) : (
+        <div>"student is not enrolled in any campus!</div>
+      )}
+      <div> edit student view </div>
     </div>
   );
-
 };
 
 export default StudentView;
